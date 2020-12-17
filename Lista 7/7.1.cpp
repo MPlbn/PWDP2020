4fd0e3d3eb3834fd2aff5a9160b0e3c08e18efbd
MPlbn <mi8ki@go2.pl>
2020-12-17 13:11:12 +0100

    Update 7.1.cpp

--- Lista 7/7.1.cpp
+++ Lista 7/7.1.cpp
@@ -4,18 +4,26 @@
 int main()
 {
 	int keyonoff = 1;
-	const int n = 5;
-	const int a = 800, b = 600, r = 150.f;
-	float x[n] = { a / 2 - r, 100, 300, 200, 250 };
-	float y[n] = { b / 2 - r, 200, 300, 100, 250 };
+	const int n = 5; //sta³a N
+	const int a = 800, b = 600, r = 20.f; // 1.wartoœæ r = 150 - ko³o to 1/4 ekranu, 2. Sta³e
+	const float g = 0.0001;
+	//próba stworzenia tablic
 
-	float vx[n] = { 0.05, 4, 0.002, 0.03, 0.5 };
-	float vy[n] = { 0.05, 2, 0.006, 0.4, 0.08 };
+	//float x[n] = { a / 2 - r, 100, 300, 200, 250 }; 
+	//float y[n] = { b / 2 - r, 200, 300, 100, 250 };
 
-	sf::RenderWindow window(sf::VideoMode(a, b), "Nasze Okno");
-	sf::VertexArray shapes(sf::CircleShape);
+	//float vx[n] = { 0.05, 4, 0.002, 0.03, 0.5 };
+	//float vy[n] = { 0.05, 2, 0.006, 0.4, 0.08 };
+	float x = a / 2 - r; //x i y kó³ka wyznaczone aby by³o wycentrowane
+	float y = b / 2 - r;
+	float vx = 0.05;
+	float vy = 0.05;
+	int dt = 0;
 	
-	shapes.setFillColor(sf::Color::Red);
+	sf::RenderWindow window(sf::VideoMode(a, b), "Nasze Okno");
+	sf::CircleShape kolo(r);
+	kolo.setFillColor(sf::Color::Red);
+	
 
 	while (window.isOpen())
 	{
@@ -31,7 +39,7 @@ int main()
 			{
 				keyonoff = 1 - keyonoff;
 			}
-			if (event.type == sf::Event::KeyPressed)
+			if (event.type == sf::Event::KeyPressed) //4. ESC zamyka program
 			{
 				if (event.key.code == sf::Keyboard::Escape)
 				{
@@ -44,21 +52,22 @@ int main()
 
 		if (keyonoff)
 		{
-			window.draw(shapes);
-			for (int i = 0; i < n; i++)
-			{
-				shapes.setPosition(x[i],y[i]);
-				if ((x[i] <= 0) || (x[i] >= (a - (2 * r))))
+			window.draw(kolo);
+			//for (int i = 0; i < n; i++)
+			//{
+				kolo.setPosition(x,y); //3. setposition
+				if ((x <= 0) || (x >= (a - (2*r)))) //5 i 6 - poruszanie siê kó³ka oraz odbijanie od œcian
 				{
-					vx[i] *= -1;
+					vx *= -1;
 				}
-				if ((y[i] <= 0) || (y[i] >= (b - (2 * r))))
+				if ((y <= 0) || (y >= (b - (2 * r))))
 				{
-					vy[i] *= -1;
+					vy *= -1;
 				}
-				x[i] += vx[i];
-				y[i] += vy[i];
-			}
+				x += vx;
+				y += vy;
+				vy += g; //Grawitacja dzia³a 8.
+			//}
 		}
 
 		window.display();
